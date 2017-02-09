d996aeb8376dcdb227842f0339329a281dd485b7
Anders Stillerud <anders.stillerud@bouvet.no>
2017-02-09 22:15:38 +0100

    Init

---
+++ sandvika-activity-board-frontend/src/main/java/no/bouvet/sandvika/SandvikaActivityBoardApplication.java
@@ -0,0 +1,12 @@
+package no.bouvet.sandvika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandvikaActivityBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SandvikaActivityBoardApplication.class, args);
+	}
+}
