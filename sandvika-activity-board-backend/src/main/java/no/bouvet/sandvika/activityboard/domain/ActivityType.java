017de601f197b259edf05d5b9508d225daad997f
Sondre Wabakken Engell <sondre.engell@bouvet.no>
2017-03-18 07:37:51 +0100

    Lagt til EBikeRide minC=0.7, kmC=0.3

--- sandvika-activity-board-backend/src/main/java/no/bouvet/sandvika/activityboard/domain/ActivityType.java
+++ sandvika-activity-board-backend/src/main/java/no/bouvet/sandvika/activityboard/domain/ActivityType.java
@@ -4,8 +4,7 @@ package no.bouvet.sandvika.activityboard.domain;
  * Enum representing the activity types we support
  */
 
-public enum ActivityType
-{
+public enum ActivityType {
     NORDIC_SKIING(Constants.NORDIC_SKIING_NAME),
     RUN(Constants.RUN_NAME),
     RIDE(Constants.RIDE_NAME),
@@ -18,11 +17,11 @@ public enum ActivityType
     WEIGHT_TRAINING(Constants.WEIGHT_TRAINING_NAME),
     KAYAKING(Constants.KAYAKING_NAME),
     ROLLER_SKI(Constants.ROLLER_SKI_NAME),
-    YOGA(Constants.YOGA_NAME);
+    YOGA(Constants.YOGA_NAME),
+    E_BIKE_RIDE(Constants.E_BIKE_RIDE_NAME);
     private String type;
 
-    ActivityType(String type)
-    {
+    ActivityType(String type) {
         this.type = type;
     }
 
@@ -33,66 +32,46 @@ public enum ActivityType
      * @return an ActivityType if a valid activityName is provided, else NULL.
      */
 
-    public static ActivityType toActivityType(String activityName)
-    {
-        if (activityName == null)
-        {
+    public static ActivityType toActivityType(String activityName) {
+        if (activityName == null) {
             return null;
-        } else if (activityName.equalsIgnoreCase(Constants.NORDIC_SKIING_NAME))
-        {
+        } else if (activityName.equalsIgnoreCase(Constants.NORDIC_SKIING_NAME)) {
             return ActivityType.NORDIC_SKIING;
-        } else if (activityName.equalsIgnoreCase(Constants.RUN_NAME))
-        {
+        } else if (activityName.equalsIgnoreCase(Constants.RUN_NAME)) {
             return ActivityType.RUN;
-        } else if (activityName.equalsIgnoreCase(Constants.RIDE_NAME))
-        {
+        } else if (activityName.equalsIgnoreCase(Constants.RIDE_NAME)) {
             return ActivityType.RIDE;
-        } else if (activityName.equalsIgnoreCase(Constants.SWIM_NAME))
-        {
+        } else if (activityName.equalsIgnoreCase(Constants.SWIM_NAME)) {
             return ActivityType.SWIM;
-        } else if (activityName.equalsIgnoreCase(Constants.ROWING_NAME))
-        {
+        } else if (activityName.equalsIgnoreCase(Constants.ROWING_NAME)) {
             return ActivityType.ROWING;
-        } else if (activityName.equalsIgnoreCase(Constants.VIRTUAL_RIDE_NAME))
-        {
+        } else if (activityName.equalsIgnoreCase(Constants.VIRTUAL_RIDE_NAME)) {
             return ActivityType.VIRTUAL_RIDE;
-        } else if (activityName.equalsIgnoreCase(Constants.HIKE_NAME))
-        {
+        } else if (activityName.equalsIgnoreCase(Constants.HIKE_NAME)) {
             return ActivityType.HIKE;
-        } else if (activityName.equalsIgnoreCase(Constants.WALK_NAME))
-        {
+        } else if (activityName.equalsIgnoreCase(Constants.WALK_NAME)) {
             return ActivityType.WALK;
-        } else if (activityName.equalsIgnoreCase(Constants.WORKOUT_NAME))
-        {
+        } else if (activityName.equalsIgnoreCase(Constants.WORKOUT_NAME)) {
             return ActivityType.WORKOUT;
-        }  else if (activityName.equalsIgnoreCase(Constants.WEIGHT_TRAINING_NAME))
-        {
+        } else if (activityName.equalsIgnoreCase(Constants.WEIGHT_TRAINING_NAME)) {
             return ActivityType.WEIGHT_TRAINING;
-        } else if (activityName.equalsIgnoreCase(Constants.KAYAKING_NAME))
-        {
+        } else if (activityName.equalsIgnoreCase(Constants.KAYAKING_NAME)) {
             return ActivityType.KAYAKING;
-        }  else if (activityName.equalsIgnoreCase(Constants.ROLLER_SKI_NAME))
-        {
+        } else if (activityName.equalsIgnoreCase(Constants.ROLLER_SKI_NAME)) {
             return ActivityType.ROLLER_SKI;
-        }  else if (activityName.equalsIgnoreCase(Constants.YOGA_NAME))
-        {
+        } else if (activityName.equalsIgnoreCase(Constants.YOGA_NAME)) {
             return ActivityType.YOGA;
-        }
-        else
-        {
+        } else {
             return null;
         }
     }
 
-    public String type()
-    {
+    public String type() {
         return this.type;
     }
 
-    public double distanceCoefficient()
-    {
-        switch (this)
-        {
+    public double distanceCoefficient() {
+        switch (this) {
             case NORDIC_SKIING:
                 return Constants.NORDIC_SKIIING_COEFFISIENT_KM;
             case RIDE:
@@ -119,15 +98,15 @@ public enum ActivityType
                 return Constants.ROLLER_SKI_COEFFISIENT_KM;
             case YOGA:
                 return Constants.YOGA_COEFFISIENT_KM;
+            case E_BIKE_RIDE:
+                return Constants.E_BIKE_RIDE_COEFFISIENT_KM;
             default:
                 return 1;
         }
     }
 
-    public double durationCoefficient()
-    {
-        switch (this)
-        {
+    public double durationCoefficient() {
+        switch (this) {
             case NORDIC_SKIING:
                 return Constants.NORDIC_SKIIING_COEFFISIENT_MIN;
             case RIDE:
@@ -154,13 +133,14 @@ public enum ActivityType
                 return Constants.ROLLER_SKI_COEFFISIENT_MIN;
             case YOGA:
                 return Constants.YOGA_COEFFISIENT_MIN;
+            case E_BIKE_RIDE:
+                return Constants.E_BIKE_RIDE_COEFFISIENT_MIN;
             default:
                 return 1;
         }
     }
 
-    private static class Constants
-    {
+    private static class Constants {
         public static final String NORDIC_SKIING_NAME = "NordicSki";
         public static final String RUN_NAME = "Run";
         public static final String RIDE_NAME = "Ride";
@@ -174,6 +154,7 @@ public enum ActivityType
         public static final String KAYAKING_NAME = "Kayaking";
         public static final String ROLLER_SKI_NAME = "RollerSki";
         public static final String YOGA_NAME = "Yoga";
+        public static final String E_BIKE_RIDE_NAME = "EBikeRide";
 
         public static final double ROWING_COEFFISIENT_KM = 1;
         public static final double KAYAKING_COEFFISIENT_KM = 1;
@@ -188,6 +169,7 @@ public enum ActivityType
         public static final double WEIGHT_TRAINING_COEFFISIENT_KM = 0;
         public static final double ROLLER_SKI_COEFFISIENT_KM = 0.5;
         public static final double YOGA_COEFFISIENT_KM = 0;
+        public static final double E_BIKE_RIDE_COEFFISIENT_KM = 0.3;
 
         public static final double ROWING_COEFFISIENT_MIN = 1;
         public static final double KAYAKING_COEFFISIENT_MIN = 1;
@@ -202,7 +184,7 @@ public enum ActivityType
         public static final double WEIGHT_TRAINING_COEFFISIENT_MIN = 1;
         public static final double ROLLER_SKI_COEFFISIENT_MIN = 1;
         public static final double YOGA_COEFFISIENT_MIN = 1;
-
+        public static final double E_BIKE_RIDE_COEFFISIENT_MIN = 0.7;
 
     }
 }
