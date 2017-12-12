82a12eed1fdb19633a95cb99fc39d7b732a3f20f
Sondre Wabakken Engell <sondrewe@gmail.com>
2017-12-12 13:33:34 +0100

    Bugfixes

--- sandvika-activity-board-backend/src/main/java/no/bouvet/sandvika/activityboard/repository/ActivityRepository.java
+++ sandvika-activity-board-backend/src/main/java/no/bouvet/sandvika/activityboard/repository/ActivityRepository.java
@@ -7,20 +7,25 @@ import java.util.stream.Stream;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-import javastrava.api.v3.model.StravaActivity;
 import no.bouvet.sandvika.activityboard.domain.Activity;
 
 @RepositoryRestResource(collectionResourceRel = "activity", path = "activity")
 public interface ActivityRepository extends MongoRepository<Activity, Integer>
 {
     public List<Activity> findByAthleteId(int id);
+
     public List<Activity> findByStartDateLocalAfter(Date startDate);
+
     public List<Activity> findByStartDateLocalBetween(Date startDate, Date endDate);
+
     public List<Activity> findById(int id);
+
     public List<Activity> findByStartDateLocalAfterAndType(Date startDate, String type);
 
     Stream<Activity> findByTypeOrderByStartDateLocalDesc(String type);
+
     List<Activity> findByStartDateLocalBetweenAndType(Date startDate, Date endDate, String type);
+
     List<Activity> findByStartDateLocalBetweenAndAthleteId(Date startDate, Date endDate, int athleteId);
 
     Stream<Activity> findAllByOrderByStartDateLocalDesc();
