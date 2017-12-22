32de77652e78ccffb669481634fec70992cdc2ed
Sondre Engell <sondrewe@gmail.com>
2017-12-22 10:20:53 +0100

    HC = 3.5 - (log10(x-1.6)*2), hvor x = aktive timer siste 30 dager

--- sandvika-activity-board-backend/src/main/java/no/bouvet/sandvika/activityboard/points/HandicapCalculator.java
+++ sandvika-activity-board-backend/src/main/java/no/bouvet/sandvika/activityboard/points/HandicapCalculator.java
@@ -113,7 +113,7 @@ public class HandicapCalculator
 
     private double calculateHandicap(double activeHours)
     {
-        double rawHc = Utils.scaledDouble(5.5 - ((Math.log10(activeHours - 2) * 3.2)), 3);
+        double rawHc = Utils.scaledDouble(3.5 - ((Math.log10(activeHours - 1.6) * 2)), 3);
 
         double hc = 0;
 
