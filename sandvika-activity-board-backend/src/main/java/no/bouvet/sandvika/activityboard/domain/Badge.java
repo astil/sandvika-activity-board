fc61d71b8b3b55e1b6f05c6273a7301e212fdd16
Sondre Engell <sondrewe@gmail.com>
2019-01-04 09:06:12 +0100

    Rulle tilbake DBRef

--- sandvika-activity-board-backend/src/main/java/no/bouvet/sandvika/activityboard/domain/Badge.java
+++ sandvika-activity-board-backend/src/main/java/no/bouvet/sandvika/activityboard/domain/Badge.java
@@ -1,12 +1,12 @@
 package no.bouvet.sandvika.activityboard.domain;
 
-import java.util.*;
-
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.DBRef;
 
-public class Badge
-{
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Badge {
     @Id
     private String name;
     private String type;
@@ -18,97 +18,78 @@ public class Badge
     private int points;
     private Set<Activity> activities;
 
-    public String getUri()
-    {
+    public String getUri() {
         return uri;
     }
 
-    public void setUri(String uri)
-    {
+    public void setUri(String uri) {
         this.uri = uri;
     }
 
-    public int getPoints()
-    {
+    public int getPoints() {
         return points;
     }
 
-    public void setPoints(int points)
-    {
+    public void setPoints(int points) {
         this.points = points;
     }
 
-    public String getName()
-    {
+    public String getName() {
         return name;
     }
 
-    public void setName(String name)
-    {
+    public void setName(String name) {
         this.name = name;
     }
 
-    public String getType()
-    {
+    public String getType() {
         return type;
     }
 
-    public void setType(String type)
-    {
+    public void setType(String type) {
         this.type = type;
     }
 
-    public String getActivityType()
-    {
+    public String getActivityType() {
         return activityType;
     }
 
-    public void setActivityType(String activityType)
-    {
+    public void setActivityType(String activityType) {
         this.activityType = activityType;
     }
 
-    public int getDistanceCriteria()
-    {
+    public int getDistanceCriteria() {
         return distanceCriteria;
     }
 
-    public void setDistanceCriteria(int distanceCriteria)
-    {
+    public void setDistanceCriteria(int distanceCriteria) {
         this.distanceCriteria = distanceCriteria;
     }
 
-    public String getTimeCriteria()
-    {
+    public String getTimeCriteria() {
         return timeCriteria;
     }
 
-    public void setTimeCriteria(String timeCriteria)
-    {
+    public void setTimeCriteria(String timeCriteria) {
         this.timeCriteria = timeCriteria;
     }
 
-    public String getBeforeOrAfter()
-    {
+    public String getBeforeOrAfter() {
         return beforeOrAfter;
     }
 
-    public void setBeforeOrAfter(String beforeOrAfter)
-    {
+    public void setBeforeOrAfter(String beforeOrAfter) {
         this.beforeOrAfter = beforeOrAfter;
     }
 
-    public Set<Activity> getActivities()
-    {
-        if (activities == null)
-        {
+    public Set<Activity> getActivities() {
+        if (activities == null) {
             activities = new HashSet<>();
         }
         return activities;
     }
 
-    public void setActivities(Set<Activity> activities)
-    {
+    public void setActivities(Set<Activity> activities) {
         this.activities = activities;
     }
 
